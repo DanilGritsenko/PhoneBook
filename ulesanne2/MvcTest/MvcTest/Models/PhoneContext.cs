085012eca4052c6e41f49ca0bdd84ab3f401700d
Opilane TTHK <opilane@tthk.ee>
2019-11-25 13:59:07 +0200

    mvc

---
+++ ulesanne2/MvcTest/MvcTest/Models/PhoneContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcTest.Models
+{
+    public class PhoneContext : DbContext
+    {
+        public PhoneContext() :
+            base("PhoneContext")
+        { }
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
---
+++ ulesanne2/MvcTest/MvcTest/Models/PhoneDBInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcTest.Models
+{
+    public class PhoneDBInitializer : DropCreateDatabaseAlways<PhoneContext>
+    {
+        protected override void Seed(PhoneContext db)
+        {
+            db.Phones.Add(new Phone { Name = "Danil", Surname = "Gritsenko", Number = "+37256687745", Email = "danil.gritsenko3@gmail.com" });
+            db.Phones.Add(new Phone { Name = "Vladimir", Surname = "Trohhalev", Number = "+37265835932", Email = "trohhalev@gmail.com" });
+            db.Phones.Add(new Phone { Name = "Daniil", Surname = "Vatsyk", Number = "+37256651325", Email = "vatsyk@gmail.com" });
+
+            base.Seed(db);
+        }
+    }
+}
